830105b2c84822b3c850f01b8e482721e3ab2db3
Brian Alexis <51463930+brrianalexis@users.noreply.github.com>
2020-10-07 20:47:31 -0300

    remove unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import Button from './components/Button';
 import Input from './components/Input';
 import List from './components/List';
