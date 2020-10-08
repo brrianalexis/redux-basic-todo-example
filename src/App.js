2c0d8c36b7e16b035415c55f51d406b0273ed437
Brian Alexis <51463930+brrianalexis@users.noreply.github.com>
2020-10-07 22:01:48 -0300

    refactor to functional components + hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from './components/Button';
 import Input from './components/Input';
 import List from './components/List';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Input />
-        <Button />
-        <List />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Input />
+      <Button />
+      <List />
+    </div>
+  );
+};
+
+export default App;
--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,39 +1,33 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { removeTask } from '../../redux/store';
 
-class ListItem extends Component {
-  handleRemove(id) {
-    const { dispatch } = this.props;
+const ListItem = () => {
+  const dispatch = useDispatch();
+  const tasks = useSelector(state => state.tasks);
+
+  const handleRemove = id => {
     dispatch(removeTask(id));
-  }
-
-  render() {
-    return (
-      <>
-        {this.props.tasks.map(task => (
-          <div style={{ display: 'flex' }} key={task.id}>
-            <li>{task.description}</li>
-            <button
-              style={{ marginLeft: '1rem' }}
-              onClick={() => this.handleRemove(task.id)}
-            >
-              <span role="img" aria-label="red cross mark">
-                ❌
-              </span>
-              Remove todo
-            </button>
-          </div>
-        ))}
-      </>
-    );
-  }
-}
-
-function mapStateToProps(state) {
-  return {
-    tasks: state.tasks,
   };
-}
 
-export default connect(mapStateToProps)(ListItem);
+  return (
+    <>
+      {tasks.map(task => (
+        <div style={{ display: 'flex' }} key={task.id}>
+          <li>{task.description}</li>
+          <button
+            style={{ marginLeft: '1rem' }}
+            onClick={() => handleRemove(task.id)}
+          >
+            <span role="img" aria-label="red cross mark">
+              ❌
+            </span>
+            Remove todo
+          </button>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default ListItem;
